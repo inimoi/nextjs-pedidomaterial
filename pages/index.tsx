0ffe3e0359a4a87681cbaede11c5977569eb2e70
Iñigo <inigo@localhost.localdomain>
2022-11-08 11:32:17 +0100

    Instalación y configuración nextUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 
 import { NextPage } from 'next'
 
+import { Button } from "@nextui-org/react";
+
 const HomePage: NextPage = () => {
   return (
     <>
       <h1>Hola Mundo</h1>
+      <Button color="gradient">
+        Aceptar
+      </Button>
     </>
   )
 }
